f77d86172da1d620d29ac5a51cdd5729c2de82d2
Muskan Gupta <muskan.gupta@knoldus.com>
2020-02-25 06:16:06 +0530

    modifying features

--- src/main/scala/com/knoldus/LogAnalysisWithAkka.scala
+++ src/main/scala/com/knoldus/LogAnalysisWithAkka.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration._
 
 class LogAnalysisWithAkka(head1: ActorRef, head2: ActorRef) extends Actor with ReadFile with ActorLogging {
   def receive: Receive = {
-    case "filesTotal" if getFiles.nonEmpty =>  head1 ! "total"
+    case "filesTotal" if getFiles.nonEmpty => head1 ! "total"
     case "fileAverage" if getFiles.nonEmpty => head2 ! "average"
     case _ => log.info("No files to do functioning")
   }
@@ -27,8 +27,9 @@ class DFG extends LogAnalysis with Actor with ActorLogging with ReadFile {
 }
 
 class D extends LogAnalysis with Actor with ActorLogging with ReadFile {
-  def receive: Receive ={
-    case "average" => average
+  def receive: Receive = {
+    case "average" => val a = average
+      sender() ! a
     case _ => log.info("Can't process")
   }
 }
@@ -38,8 +39,8 @@ object LogAnalyzer extends App {
   val system = ActorSystem("LogAnalyzerSystem")
   val head1 = system.actorOf(Props[DFG], name = "head1")
   val head2 = system.actorOf(Props[D], name = "head2")
-  val head = system.actorOf(Props(new LogAnalysisWithAkka(head1,head2)), name = "head")
-  implicit val timeout: Timeout = Timeout(10.seconds)
+  val head = system.actorOf(Props(new LogAnalysisWithAkka(head1, head2)), name = "head")
+  implicit val timeout: Timeout = Timeout(50.seconds)
 
   val total = head ? "filesTotal"
   total.map(identity)
